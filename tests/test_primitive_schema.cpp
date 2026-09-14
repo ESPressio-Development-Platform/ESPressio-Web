c8297d8b08dbd63f370bc49a205fbafa08bafbf8
Simon J Stuart <lakraven@gmail.com>
2026-09-14 10:29:21 +0200

    Cross-check real Serializable Command discovery

--- tests/test_primitive_schema.cpp
+++ tests/test_primitive_schema.cpp
@@ -3,6 +3,9 @@
 #include <cstdint>
 
 #include <ESPressio_WebPrimitiveSchema.hpp>
+#include <ESPressio_SerializableCommand.hpp>
+#include <ESPressio_SerializationMacros.hpp>
+#include <ESPressio_TypeDirectory.hpp>
 
 using namespace ESPressio;
 using namespace ESPressio::Web;
@@ -14,6 +17,18 @@ const Serializable::StaticSchemaDescriptor PrimarySchema{
 };
 const Serializable::StaticSchemaDescriptor SecondarySchema{
     1, 1, 1, 0, nullptr, 0x5678u, 4, 8, 16
+};
+
+struct SchemaCommand final : Command::SerializableCommand<SchemaCommand> {
+    static constexpr Command::CommandTypeId TypeId{201};
+    static constexpr const char* CanonicalName="Test.Web.SchemaCommand";
+    static constexpr std::size_t MaximumLiveInstances=2;
+    static constexpr std::size_t MaximumPendingExecutions=1;
+    using ExecutionAdmissionPolicy=Command::RequiredExecution;
+    std::int32_t Value=0;
+    ESPRESSIO_SERIALIZABLE_TYPE(SchemaCommand)
+    ESPRESSIO_SERIALIZABLE_SCHEMA_VERSION(1)
+    ESPRESSIO_SERIALIZABLE_PROPERTIES(ESPRESSIO_PROPERTY("value",Value))
 };
 
 Event::EventDynamicDispatchResult EventSubmit(
@@ -94,6 +109,27 @@ void TestCommandMetadata() {
     assert(metadata.DynamicCapability == PrimitiveDynamicCapability::CommandRequesterRequired);
 }
 
+void TestRealSerializableCommandDescriptor() {
+    const auto descriptor=SchemaCommand::GetPrimitiveTypeDescriptor();
+    assert(descriptor.IsValid());
+    assert(descriptor.Capabilities.Contains(Primitive::PrimitiveTypeCapability::Serializable));
+    assert(descriptor.SerializedSize.MaximumCompletePrimitiveWireBytes>0);
+
+    Primitive::TypeDirectory<1> directory;
+    assert(directory.Register<SchemaCommand>()==Primitive::TypeDirectoryRegistrationStatus::Success);
+    assert(directory.Initialize()==Primitive::TypeDirectoryInitializationStatus::Success);
+    const auto view=directory.View();
+    const auto* discovered=view.Find({Primitive::FamilyIds::Command,SchemaCommand::TypeId.Value()});
+    assert(discovered);
+
+    PrimitiveSchemaMetadata metadata;
+    const auto result=DescribePrimitiveSchema(*discovered,metadata);
+    assert(result);
+    assert(metadata.PrimarySchema!=nullptr);
+    assert(metadata.DynamicCapability==PrimitiveDynamicCapability::CommandFireAndForget);
+    assert(metadata.MaximumCompletePrimitiveWireBytes==descriptor.SerializedSize.MaximumCompletePrimitiveWireBytes);
+}
+
 void TestStateIsReadOnly() {
     State::StateTypeDescriptor extension{};
     extension.TypeId = State::StateTypeId{13};
@@ -132,6 +168,7 @@ void TestFailClosedFamilyInterpretation() {
 int main() {
     TestEventMetadata();
     TestCommandMetadata();
+    TestRealSerializableCommandDescriptor();
     TestStateIsReadOnly();
     TestFailClosedFamilyInterpretation();
     return 0;
